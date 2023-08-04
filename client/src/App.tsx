8f1a93aec20d0ad10262880df28eaeef97bd3852
joe-shajan <joeshajan1551@gmail.com>
2023-08-04 18:21:36 +0530

    tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,4 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { axiosClient } from "./lib/axios";
 
 function App() {
@@ -27,17 +24,8 @@ function App() {
       <div>
         {loading && <p>Loading...</p>}
         {error && <p>Error: {error.message}</p>}
-        {data && <div>{data}</div>}
+        {data && <h1 className="text-3xl font-bold">{data}</h1>}
       </div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
     </>
   );
 }
